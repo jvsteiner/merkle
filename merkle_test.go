1215cbe5e98a791375319ffc63591572706e15f3
Jamie Steiner <jvsteiner@gmail.com>
2017-06-02 15:31:47 +0300

    flattened structure

--- merkle_test.go
+++ merkle_test.go
@@ -1,12 +1,10 @@
-package test_merkle
+package merkle
 
 import (
 	"crypto/sha256"
 	"encoding/hex"
 	"runtime"
 	"testing"
-
-	"github.com/jvsteiner/merkle"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -28,17 +26,17 @@ func NewTestDigests() (t [][32]byte) {
 }
 
 func TestBuildData(t *testing.T) {
-	tree := merkle.NewTreeFromData(NewTestData())
+	tree := NewTreeFromData(NewTestData())
 	assert.True(t, hex.EncodeToString(tree.Root.Digest[:]) == "14ede5e8e97ad9372327728f5099b95604a39593cac3bd38a343ad76205213e7", "Wrong root calculated from Data")
 }
 
 func TestBuildDigests(t *testing.T) {
-	tree := merkle.NewTreeFromDigests(NewTestDigests())
+	tree := NewTreeFromDigests(NewTestDigests())
 	assert.True(t, hex.EncodeToString(tree.Root.Digest[:]) == "14ede5e8e97ad9372327728f5099b95604a39593cac3bd38a343ad76205213e7", "Wrong root calculated from Digests")
 }
 
 func TestRelationships(t *testing.T) {
-	tree := merkle.NewTreeFromData(NewTestData())
+	tree := NewTreeFromData(NewTestData())
 	assert.True(t, tree.Leaves[0].LeftSide, "Node 0 is left side")
 	assert.True(t, tree.Leaves[2].LeftSide, "Node 2 is left side")
 	assert.False(t, tree.Leaves[1].LeftSide, "Node 1 is left side")
@@ -47,7 +45,7 @@ func TestRelationships(t *testing.T) {
 }
 
 func TestGetChain(t *testing.T) {
-	tree := merkle.NewTreeFromData(NewTestData())
+	tree := NewTreeFromData(NewTestData())
 	chain, err := tree.GetChain(0)
 	assert.True(t, err == nil, "Single Chain should be returned")
 	assert.True(t, len(chain) == 3, "Chain should have length == 3")
@@ -61,11 +59,11 @@ func TestGetChain(t *testing.T) {
 }
 
 func TestAddAdjust(t *testing.T) {
-	controlTree := merkle.NewTreeFromData(NewTestData())
-	testTree := merkle.NewTreeFromData(NewTestData()[0:1])
-	testTree.AddAdjust(merkle.NewNode([]byte("b")))
-	testTree.AddAdjust(merkle.NewNode([]byte("c")))
-	testTree.AddAdjust(merkle.NewNode([]byte("d")))
+	controlTree := NewTreeFromData(NewTestData())
+	testTree := NewTreeFromData(NewTestData()[0:1])
+	testTree.AddAdjust(NewNode([]byte("b")))
+	testTree.AddAdjust(NewNode([]byte("c")))
+	testTree.AddAdjust(NewNode([]byte("d")))
 	assert.True(t, controlTree.Root.Hexdigest() == testTree.Root.Hexdigest(), "Control Tree should have same root val as testTree")
 
 }
@@ -76,14 +74,14 @@ func BenchmarkFromDigest(b *testing.B) {
 		t = append(t, sha256.Sum256([]byte("a")))
 	}
 	b.ResetTimer()
-	merkle.NewTreeFromDigests(t)
+	NewTreeFromDigests(t)
 }
 
 func BenchmarkAddAdjust(b *testing.B) {
-	t := merkle.NewTreeFromData(NewTestData()[0:1])
-	s := []*merkle.Node{}
+	t := NewTreeFromData(NewTestData()[0:1])
+	s := []*Node{}
 	for i := 0; i < b.N; i++ {
-		s = append(s, merkle.NewNode([]byte("a")))
+		s = append(s, NewNode([]byte("a")))
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
